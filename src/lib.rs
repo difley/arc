51dd36b07abfc98685c332364dc14d2f653d0e26
Seth Difley <difley@gmail.com>
2020-04-12 22:46:35 -0500

    A test is passing that should not pass.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,46 @@
-#[derive(Debug)]
+#[macro_use]
+extern crate approx;
+
+use approx::AbsDiffEq;
+use approx::RelativeEq;
+use approx::UlpsEq;
+use std::f32;
+
+#[derive(Debug, PartialEq)]
 struct Point {
     x: f32,
     y: f32,
 }
 
-fn line_point(origin: &Point, angle: &f32, distance: &f32) {
+impl AbsDiffEq for Point {
+
+    type Epsilon = f32;
+
+    fn default_epsilon() -> f32 {
+        f32::EPSILON
+    }
+
+    fn abs_diff_eq(&self, other: &Self, epsilon: f32) -> bool {
+        f32::abs_diff_eq(&self.x, &other.x, epsilon) &&
+        f32::abs_diff_eq(&self.y, &other.y, epsilon)
+    }
+}
+
+impl RelativeEq for Point {
+    fn default_max_relative() -> f32 {
+        1.
+    }
+
+    fn relative_eq(&self, other: &Self, epsilon: f32, max_relative: f32) -> bool {
+        f32::relative_eq(&self.x, &other.x, epsilon, max_relative) &&
+        f32::relative_eq(&self.y, &other.y, epsilon, max_relative)
+    }
+}
+
+fn line_point(origin: &Point, angle: &f32, distance: &f32, t: &f32) -> Point {
+    let x = origin.x + distance*angle.cos()*t;
+    let y = origin.y + distance*angle.sin()*t;
+    return Point {x, y};
 }
  
 #[cfg(test)]
@@ -12,13 +48,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
     fn line_point_doesnt_explode() {
-        let myPoint = Point { x: 2., y: 3. };
-        assert_eq!(line_point(&myPoint, &0.4, &2.), ());
+        let my_point = Point { x: 2., y: 3. };
+        abs_diff_eq!(line_point(&my_point, &0.4, &2., &1.), Point {x: 1., y: 2.}, epsilon = f32::EPSILON);
     }
 }
