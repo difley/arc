3b7fb2dad13e076ee5eea4e12ab9ae0054c0b995
Seth Difley <difley@gmail.com>
2020-04-15 23:20:51 -0500

    Added four line tests.

--- src/lib.rs
+++ src/lib.rs
@@ -45,13 +45,29 @@ fn line_point(origin: &Point, angle: &f32, distance: &f32, t: &f32) -> Point {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use f32::consts;
 
     #[test]
-    fn line_point_doesnt_explode() {
-        let my_point = Point { x: 2., y: 3. };
-        let calculated_point = line_point(&my_point, &0.4, &2., &1.);
-        println!("my_point: {:?}", &my_point);
-        println!("calculated_point: {:?}", &calculated_point);
-        assert_abs_diff_eq!(calculated_point, Point {x: 3.842122, y: 3.7788367})
+    fn vertical_line_at_origin() {
+        let my_point = Point { x: 0., y: 0. };
+        assert_abs_diff_eq!(line_point(&my_point, &(consts::PI/2.), &1., &1.), Point {x: 0., y: 1.});
+    }
+
+    #[test]
+    fn horizontal_line_at_origin() {
+        let my_point = Point { x: 0., y: 0. };
+        assert_abs_diff_eq!(line_point(&my_point, &consts::PI, &1., &1.), Point { x: -1., y: 0. });
+    }
+
+    #[test]
+    fn halfway_between_vertical_and_horizontal() {
+        let my_point = Point { x: 0., y: 0. };
+        assert_abs_diff_eq!(line_point(&my_point, &(consts::PI/4.), &1., &1.), Point { x: 1./f32::sqrt(2.), y: 1./f32::sqrt(2.) });
+    }
+
+    #[test]
+    fn translated_vertical_line() {
+        let my_point = Point { x: 5.5, y: 1.1 };
+        assert_abs_diff_eq!(line_point(&my_point, &(consts::PI/2.), &1., &1.), Point { x: 5.5, y: 2.1 });
     }
 }
